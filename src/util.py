8bbb91ba436e7f1b49c757aedcd3aa53763e7862
Jono Shields <jonathonshields@gmail.com>
2025-05-03 23:57:09 +0000

    fix dates

--- src/util.py
+++ src/util.py
@@ -3,6 +3,6 @@ from datetime import datetime
 DAYS = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
 
 def letters_day(date):
-    "%Y-%m-%dT%H:%M"
+    date_format = "%Y-%m-%dT%H:%M"
     d = datetime.strptime(date, date_format)
     return DAYS[d.weekday()]
