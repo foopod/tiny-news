e89629982eb1706b9d5e05e433e42fb16a4fb0ea
Jono Shields <jonathonshields@gmail.com>
2025-08-11 20:12:21 +1200

    add easy sudokus

--- src/scripts/sudoku.py
+++ src/scripts/sudoku.py
@@ -1,23 +1,28 @@
 import json
 from datetime import datetime, timedelta
+import random
 
 def process_sudoku():
-    with open(f"SUPER160.txt", 'r') as file:
-        current_date = datetime.now()
-        for line in file:
-            line = line.strip()
+    with open(f"scripts/sudoku17.txt", 'r') as file:
+        all_lines = file.readlines()
+
+    current_date = datetime.now()
+    lines = random.sample(all_lines, 300)
+    for line in lines:
+        line = line.strip()
+    
+        puzzle = {"puzzle_type": "sudoku", "data": {"task": line}}
+
+
+        is_thursay = False
+        while (not is_thursay):
+            current_date += timedelta(days=1)
+            if(current_date.weekday() == 1): # Thusday
+                is_thursay = True
+
+        new_file_name = current_date.strftime("%Y-%m-%d")
         
-            puzzle = {"puzzle_type": "sudoku", "data": {"task": line}}
+        with open(f"test/{new_file_name}.json", 'w') as file:
+            file.write(json.dumps(puzzle))
 
-
-            is_tuesday = False
-            while (not is_tuesday):
-                current_date += timedelta(days=1)
-                if(current_date.weekday() == 1): # Tuesday
-                    is_tuesday = True
-
-            new_file_name = current_date.strftime("%Y-%m-%d")
-            
-            with open(f"test/{new_file_name}.json", 'w') as file:
-                file.write(json.dumps(puzzle))
-
+process_sudoku()
